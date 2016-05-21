135d70a6339dc8b870fc8adb8bd22e7b65d6a909
TomMJG <912135959@qq.com>
2016-05-21 13:30:23 +0800

    Initial Commit

---
+++ TW-9-晚八点后打折/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TW-9-晚八点后打折
+//
+//  Created by 马家固 on 16/5/21.
+//  Copyright © 2016年 马家固. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
